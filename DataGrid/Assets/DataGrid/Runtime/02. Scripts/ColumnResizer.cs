fa942bef6de5fdc0d33923def979f63842b59712
Soung-Gyu Jin <lunasia819@gmail.com>
2024-05-07 14:04:28 +0900

    Test

---
+++ DataGrid/Assets/DataGrid/Runtime/02. Scripts/ColumnResizer.cs
@@ -0,0 +1,71 @@
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UniRx;
+
+public class ColumnResizer : MonoBehaviour, IDragHandler, IBeginDragHandler
+{
+    public RectTransform leftColumn;    // 왼쪽 패널
+    
+    public float minWidth = 100f;       // 최소 너비
+    public float maxWidth = 1000f;      // 최대 너비 
+
+    private Button resizerButton;       // 리사이저 버튼
+    private float originalX;            // 드래그 시작 시의 초기 X 위치
+    private float initialLeftWidth;     // 드래그 시작 시 왼쪽 패널의 초기 너비
+
+    private void Start()
+    {
+        resizerButton = GetComponent<Button>();
+
+        if (leftColumn == null)
+        {
+            Debug.LogError("Panels or buttons are not assigned");
+            return;
+        }
+        resizerButton.OnClickAsObservable()
+            .Buffer(resizerButton.OnClickAsObservable().ThrottleFirst(TimeSpan.FromSeconds(0.7f)))
+            .Where(x => x.Count >= 2)
+            .Subscribe(_ => OnDoubleClick());
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        originalX = this.transform.localPosition.x; // 드래그 시작 시의 위치 저장
+        initialLeftWidth = leftColumn.rect.width; // 드래그 시작 시 왼쪽 패널의 너비 저장
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Vector2 localPoint;
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            (RectTransform)this.transform.parent, eventData.position, eventData.pressEventCamera, out localPoint))
+        {
+            float newX = localPoint.x - originalX;  // 원점으로부터의 변화량 계산
+
+            // 왼쪽 패널의 새로운 너비 계산
+            float newWidth = Mathf.Clamp(initialLeftWidth + newX, minWidth, maxWidth);
+            leftColumn.sizeDelta = new Vector2(newWidth, leftColumn.sizeDelta.y);
+
+            // Layout Group 갱신, leftPanel의 레이아웃을 갱신
+            LayoutRebuilder.ForceRebuildLayoutImmediate(leftColumn);
+            NotifySizeChanged(newWidth);
+        }
+    }
+
+    private void OnDoubleClick()
+    {
+        // TODO : 모든 rows 기준의 최대 preferredWidth로 수정
+    }
+
+    private void NotifySizeChanged(float newSize)
+    {
+        ColumnInfo column = leftColumn.GetComponent<ColumnInfo>();
+        column.Width = newSize;
+        column.ListView.ListViewWidthCal(column.ColumnIndex);
+
+        //leftPanel.// TODO : Header에서 Column1의 Info 알아낸 다음 Width 변경
+        // Resizer 옆에 있는 Column이 몇번째 Column인지 모르는 문제.
+    }
+}
---
+++ DataGrid/Assets/DataGrid/Runtime/02. Scripts/DataGridTest.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class DataGridTest : MonoBehaviour
+{
+    [SerializeField] private ListView listView;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Button button = this.GetComponent<Button>();
+        button.onClick.AddListener(AddRowTest);
+    }
+
+    private void AddRowTest()
+    {
+        listView.UseColumnResizer = true;
+        listView.AddColumn("날짜",       width: 470f, fontSize: 50f);
+        listView.AddColumn("이름",       width: 328f, fontSize: 50f);
+        listView.AddColumn("소속",       width: 192f, fontSize: 50f);
+        listView.AddColumn("사번",       width: 285f, fontSize: 50f);
+        listView.AddColumn("확인",       width: 336f, fontSize: 50f);
+        listView.AddColumn("삭제",       width: 336f, fontSize: 50f);
+
+        
+        UnityAction unityAction1 = new UnityAction(() => { Debug.Log("확인1"); });
+        UnityAction unityAction2 = new UnityAction(() => { Debug.Log("삭제1"); });
+
+        listView.AddRow("2024-05-07 22:11", "김철수", "관리팀", "20240001", ("확인", unityAction1), ("삭제", unityAction2));
+    }
+}
+
